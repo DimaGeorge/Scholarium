e6c53731a114411979e82b01a980edc0ad73df00
Jitariu <jitariuandrei19@gmail.com>
2018-04-30 18:26:54 +0300

    verification ++

--- verification.py
+++ verification.py
@@ -1,5 +1,5 @@
 import settings
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from werkzeug.wrappers import Response
 import sys
 import hashlib
@@ -21,12 +21,20 @@ def verifyCertificate():
     plainCertificate = verificationForm['cert']
     sha256 = hashlib.sha256()
     sha256.update(plainCertificate)
-    myHashedCertificate = sha256.hexdigest()
+    myHashedCertificate = 'dada'
+
 
     if transactionOutput:
         hashedCertificate = settings.multichainNode.gettxoutdata(transaction,1)
         if hashedCertificate == myHashedCertificate:
-            return 'The certificate is valid'
+            transactionData = settings.multichainNode.getrawtransaction(transaction,1)
+            
+            transactionDetails = {}
+            transactionDetails['Message'] = 'The certificate is valid'
+            transactionDetails['Owner'] = transactionData['vout'][0]['scriptPubKey']['addresses']
+            transactionDetails['Sender'] = transactionData['vout'][2]['scriptPubKey']['addresses'] 
+            transactionDetails['HashedCertificate'] = hashedCertificate
+            return jsonify(transactionDetails)
         else:
             return 'The certificates do not match'    
     else:    
